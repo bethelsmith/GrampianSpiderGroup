961f021c3a027a3b4d41db9225e3bda70bf7fda0
Bethel Smith <bethelsmith@hotmail.com>
2012-03-03 15:09:54 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GrampianSpiderGroup::Application.config.secret_token = 'd46305e1b95264ded08f205a840bc848b6e6b817308e9dbd3cdf9860bba1952cba891af963b0ba379ab66ab985c37552fe94d8742bd326b1ca1f94b2fb74d1a7'
